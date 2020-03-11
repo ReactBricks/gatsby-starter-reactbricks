48d398343b8e69d5f86ded29fbd6f93dfbb36133
Matteo Frana <f2@f2.net>
2020-03-11 17:18:17 +0100

    Header responsive; aggiornato packages

--- src/components/header.js
+++ src/components/header.js
@@ -4,40 +4,22 @@ import React from 'react'
 import gatsbyLogo from '../images/gatsby-icon.png'
 import rbLogo from '../images/reactbricks-icon.svg'
 
+import './header.css'
+
 const Header = () => (
-  <header
-    style={{
-      paddingTop: '2rem',
-      paddingBottom: '2rem',
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 896,
-      }}
-    >
-      <div
-        style={{
-          width: '100%',
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <img src={rbLogo} style={{ width: 40 }} alt="React Bricks" />
-          <img
-            src={gatsbyLogo}
-            style={{ width: 40, marginLeft: '1rem' }}
-            alt="Gatsby"
-          />
+  <header>
+    <div className="container">
+      <div className="header-menu">
+        <div className="left-menu">
+          <div className="logos">
+            <img src={rbLogo} alt="React Bricks" />
+            <img
+              src={gatsbyLogo}
+              style={{ marginLeft: '1rem' }}
+              alt="Gatsby"
+            />
+          </div>
           <h1
-            style={{
-              fontSize: '1.25rem',
-              marginLeft: '1rem',
-            }}
           >
             <Link
               to="/"
@@ -49,11 +31,7 @@ const Header = () => (
             </Link>
           </h1>
         </div>
-        <Link
-          to="/admin"
-        >
-          Admin Dashboard &raquo;
-        </Link>
+        <Link to="/admin">Admin Dashboard &raquo;</Link>
       </div>
     </div>
   </header>
