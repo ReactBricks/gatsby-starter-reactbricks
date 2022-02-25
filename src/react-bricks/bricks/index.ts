4fdca1a1e7b0c3b16942884fc68029b09149e9b8
Giorgio Tognoli <giorgio@f2.net>
2022-02-25 18:46:29 +0100

    Updated packages (react-bricks v.3.2.12)

--- src/react-bricks/bricks/index.ts
+++ src/react-bricks/bricks/index.ts
@@ -1,8 +1,10 @@
 import { types } from 'react-bricks/frontend'
 import HeroUnit from './MyHeroUnit'
+import website from 'react-bricks-ui/website'
 
 const bricks: types.Brick<any>[] = [
   HeroUnit, // Example brick
+  ...website,
   // Put here your other bricks...
 ]
 
