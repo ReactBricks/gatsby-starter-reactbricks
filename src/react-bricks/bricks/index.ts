c3c4d5780347b562e2aa6456ded073527f380ef7
Giorgio Tognoli <giorgio@f2.net>
2022-05-26 15:31:26 +0200

    removed react-bricks-ui

--- src/react-bricks/bricks/index.ts
+++ src/react-bricks/bricks/index.ts
@@ -1,13 +1,9 @@
 import { types } from 'react-bricks/frontend'
 import HeroUnit from './MyHeroUnit'
-import website from 'react-bricks-ui/website'
-import blog from 'react-bricks-ui/blog'
 
 const bricks: types.Brick<any>[] = [
-  HeroUnit, // Example brick
-  ...blog,
-  ...website,
   // Put here your other bricks...
+  HeroUnit, // Example brick
 ]
 
 export default bricks
