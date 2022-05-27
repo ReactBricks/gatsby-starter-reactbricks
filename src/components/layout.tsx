77fff72fddb1e08862f52acef306d88040bfc517
Giorgio Tognoli <giorgio@f2.net>
2022-05-27 17:34:01 +0200

    add layout interface

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 
 import Header from './header'
 import Footer from './footer'
+interface LayoutProps {
+  children?: ReactNode
+}
 
-const Layout: React.FC = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col h-screen justify-between antialiased">
       <Header />
