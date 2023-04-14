e35eea703a359a58f62fafe7d7efdc598a93c17a
FrancescoMottaDev <francesco.motta@reactbricks.com>
2023-04-14 16:00:47 +0200

    fixed require

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
-const config = require('./src/react-bricks/config')
+const config = require.resolve('./src/react-bricks/config.tsx')
 const bluebird = require('bluebird')
 const fetchPages = require('react-bricks/frontend').fetchPages
 const fetchPage = require('react-bricks/frontend').fetchPage
