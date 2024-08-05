e35a0e3e7cd91d899ee5d079f471c1cabcbbe478
Cecilia Panicali <cecilia@f2.net>
2024-08-05 12:24:37 +0200

    Update to react-bricks v4.4.5 and set experimentalSidebarRepeaterItems to "true" in config.

--- src/react-bricks/config.tsx
+++ src/react-bricks/config.tsx
@@ -31,6 +31,7 @@ const config: types.ReactBricksConfig = {
   disableSaveIfInvalidProps: false,
   enablePreview: true,
   enablePreviewImage: true,
+  experimentalSidebarRepeaterItems: true
 }
 
 export default config
